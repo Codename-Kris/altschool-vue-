261d4109e4ab32975f02734d8d2c61724e7c645a
Benjamin Nomba <bnomba0704@gmail.com>
2023-02-05 14:12:26 -0800

    new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   src/App.vue 	new file:   src/assets/base.css 	new file:   src/assets/logo.svg 	new file:   src/assets/main.css 	new file:   src/components/Bookings.vue 	new file:   src/components/Color.vue 	new file:   src/components/ColorPickerGame.vue 	new file:   src/components/CustomFetch.vue 	new file:   src/components/Recap_01_02_2023.vue 	new file:   src/components/WelcomeBox.vue 	new file:   src/composables/GameComp.js 	new file:   src/composables/book-manager.js 	new file:   src/layouts/Auth.vue 	new file:   src/layouts/Dashboard.vue 	new file:   src/main.js 	new file:   src/pages/About.vue 	new file:   src/pages/Home.vue 	new file:   src/pages/Notfound.vue 	new file:   src/pages/Post.vue 	new file:   vite.config.js 	new file:   yarn.lock

---
+++ src/components/ColorPickerGame.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Color Picker Game</h1>
+  <div>{{ message }}</div>
+  <button v-for="color in colors" :key="color" @click="matchColor(color)">
+    {{ color }}
+  </button>
+</template>
+
+<script>
+import GameComp from "../composables/GameComp.js";
+
+export default {
+  setup() {
+    const { colors, message, matchColor } = GameComp();
+    return { colors, message, matchColor };
+  },
+};
+</script>
+
+<style></style>
+
