55b07efe7ca06afe808d38a719d5eb38f3473ab2
Christian Ofoka <104909904+Codename-Kris@users.noreply.github.com>
2023-02-07 22:21:54 +0100

    Document committed hhgggffff

---
+++ src/composables/ColorPicker.js
@@ -0,0 +1,20 @@
+import { ref } from "@vue/reactivity";
+
+export default function ColorPicker() {
+  const colors = ["green", "red", "blue", "purple"];
+  let message = ref("Pick a color...");
+
+  const matchColor = (value) => {
+    // do a random color based on the array index;
+    const randomNumber = Math.floor(Math.random() * colors.length);
+
+    if (colors[randomNumber] === value) {
+      message.value = `You win... [answer: ${colors[randomNumber]}]`;
+      return;
+    }
+
+    message.value = `You loose [answer: ${colors[randomNumber]}]`;
+  };
+
+  return { colors, message, matchColor };
+}
